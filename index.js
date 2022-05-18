f6afc1ddfb2ed8cbee13a244e54d4c3600352d12
林思恺 <linsk.vip@qq.com>
2022-05-18 16:56:42 +0800

    1.0.3

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ var estreeWalker = require('estree-walker');
 var MagicString = require('magic-string');
 var pluginutils = require('@rollup/pluginutils');
 
-var isReference = function (node, parent) {
-	if (node.type === 'MemberExpression') {
+var isReference = function(node, parent) {
+	if(node.type === 'MemberExpression') {
 		return !node.computed && isReference(node.object, node);
 	}
 
-	if (node.type === 'Identifier') {
+	if(node.type === 'Identifier') {
 		// TODO is this right?
-		if (parent.type === 'MemberExpression') { return parent.computed || node === parent.object; }
+		if(parent.type === 'MemberExpression') { return parent.computed || node === parent.object; }
 
 		// disregard the `bar` in { bar: foo }
-		if (parent.type === 'Property' && node !== parent.value) { return false; }
+		if(parent.type === 'Property' && node !== parent.value) { return false; }
 
 		// disregard the `bar` in `class Foo { bar () {...} }`
-		if (parent.type === 'MethodDefinition') { return false; }
+		if(parent.type === 'MethodDefinition') { return false; }
 
 		// disregard the `bar` in `export { foo as bar }`
-		if (parent.type === 'ExportSpecifier' && node !== parent.local) { return false; }
+		if(parent.type === 'ExportSpecifier' && node !== parent.local) { return false; }
 
 		// disregard the `bar` in `import { bar as foo }`
-		if (parent.type === 'ImportSpecifier' && node === parent.imported) { return false; }
+		if(parent.type === 'ImportSpecifier' && node === parent.imported) { return false; }
 
 		return true;
 	}
@@ -30,11 +30,11 @@ var isReference = function (node, parent) {
 	return false;
 };
 
-var flatten = function (startNode) {
+var flatten = function(startNode) {
 	var parts = [];
 	var node = startNode;
 
-	while (node.type === 'MemberExpression') {
+	while(node.type === 'MemberExpression') {
 		parts.unshift(node.property.name);
 		node = node.object;
 	}
@@ -45,53 +45,53 @@ var flatten = function (startNode) {
 	return { name: name, keypath: parts.join('.') };
 };
 function polyfill(options) {
-	if (!options) { throw new Error('Missing options'); }
+	if(!options) { throw new Error('Missing options'); }
 	var filter = pluginutils.createFilter(options.include, options.exclude);
 	var modules = options.modules;
 	var modulesMap = new Map();
-	var membersMap=new Map();
+	var membersMap = new Map();
 
-	var coreJsConfig=options['core-js'];
-	if(coreJsConfig){
-		require("./build/core-js")(coreJsConfig,membersMap,modulesMap);
+	var coreJsConfig = options['core-js'];
+	if(coreJsConfig) {
+		require("./build/core-js")(coreJsConfig, membersMap, modulesMap);
 	}
-	var skyCoreConfig=options['sky-core'];
-	if(skyCoreConfig){
-		require("./build/sky-core")(skyCoreConfig,membersMap,modulesMap);
+	var skyCoreConfig = options['sky-core'];
+	if(skyCoreConfig) {
+		require("./build/sky-core")(skyCoreConfig, membersMap, modulesMap);
 	}
-	if(modules){
-		Object.entries(modules).forEach(function([key,value]){
+	if(modules) {
+		Object.entries(modules).forEach(function([key, value]) {
 			//console.log(key);
-			if(key.startsWith(".")){
-				membersMap.set(key,value);
-			}else{
-				modulesMap.set(key,value);
+			if(key.startsWith(".")) {
+				membersMap.set(key, value);
+			} else {
+				modulesMap.set(key, value);
 			}
-		})
+		});
 	}
 
 	var sourceMap = options.sourceMap !== false && options.sourcemap !== false;
 	return {
 		name: "polyfill",
 		transform(code, id) {
-			if (!filter(id)) { return null; }
+			if(!filter(id)) { return null; }
 			var ast = null;
 			try {
 				ast = this.parse(code);
-			} catch (err) {
+			} catch(err) {
 				this.warn({
 					code: 'PARSE_ERROR',
 					message: ("rollup-plugin-inject: failed to parse " + id + ". Consider restricting the plugin to particular files via options.include")
 				});
 			}
-			if (!ast) {
+			if(!ast) {
 				return null;
 			}
 			//console.log(JSON.stringify(ast, null, 1));
 			var imports = new Set();
-			ast.body.forEach(function (node) {
-				if (node.type === 'ImportDeclaration') {
-					node.specifiers.forEach(function (specifier) {
+			ast.body.forEach(function(node) {
+				if(node.type === 'ImportDeclaration') {
+					node.specifiers.forEach(function(specifier) {
 						//console.log(specifier.local.name);
 						imports.add(specifier.local.name);
 					});
@@ -103,11 +103,10 @@ function polyfill(options) {
 			var newImports = new Set();
 
 			function handleReference(node, name, keypath) {
-				modulesMap.forEach(function (mod, path) {
-					//console.log(path,keypath);
-					if (path === keypath && !scope.contains(name)) {
-						if (Array.isArray(mod)) {
-							mod.forEach(function (mod) {
+				membersMap.forEach(function(mod, key) {
+					if(keypath.endsWith(key)) {
+						if(Array.isArray(mod)) {
+							mod.forEach(function(mod) {
 								newImports.add(mod);
 							});
 						} else {
@@ -115,21 +114,9 @@ function polyfill(options) {
 						}
 					}
 				});
-				return false;
-			}
-			function handleMember(startNode){
-				var parts = [];
-				var node = startNode;
-			
-				while (node.type === 'MemberExpression') {
-					parts.unshift(node.property.name);
-					node = node.object;
-				}
-			
-				if(parts.length){
-					var keypath="."+parts.join(".");
-					membersMap.forEach(function(mod, key) {
-						if(keypath.endsWith(key)) {
+				modulesMap.forEach(function(mod, path) {
+					if(mod && !imports.has(name) && !scope.contains(name)) {
+						if(path === keypath && !scope.contains(name)) {
 							if(Array.isArray(mod)) {
 								mod.forEach(function(mod) {
 									newImports.add(mod);
@@ -138,57 +125,57 @@ function polyfill(options) {
 								newImports.add(mod);
 							}
 						}
-					});
-				}
+					}
+				});
+				return false;
 			}
 
 			estreeWalker.walk(ast, {
 				enter: function enter(node, parent) {
 					//console.log(node);
-					if (sourceMap) {
+					if(sourceMap) {
 						magicString.addSourcemapLocation(node.start);
 						magicString.addSourcemapLocation(node.end);
 					}
-					if (node.scope) {
+					if(node.scope) {
 						scope = node.scope; // eslint-disable-line prefer-destructuring
 					}
-					handleMember(node);
 					// special case – shorthand properties. because node.key === node.value,
 					// we can't differentiate once we've descended into the node
-					if (node.type === 'Property' && node.shorthand) {
+					if(node.type === 'Property' && node.shorthand) {
 						var ref = node.key;
 						var name = ref.name;
 						handleReference(node, name, name);
 						this.skip();
 						return;
 					}
-					if (node.type === 'ImportExpression') {
+					if(node.type === 'ImportExpression') {
 						handleReference(node, "import", "import");
 						this.skip();
 						return;
 					}
-					if (isReference(node, parent)) {
-						var { keypath, name } = flatten(node);
+					if(isReference(node, parent)) {
+						var { name, keypath } = flatten(node);
 						var handled = handleReference(node, name, keypath);
-						if (handled) {
+						if(handled) {
 							this.skip();
 						}
 					}
 				},
 				leave: function leave(node) {
-					if (node.scope) {
+					if(node.scope) {
 						scope = scope.parent;
 					}
 				}
 			});
-			if (newImports.size === 0) {
+			if(newImports.size === 0) {
 				return {
 					code: code,
 					ast: ast,
 					map: sourceMap ? magicString.generateMap({ hires: true }) : null
 				};
 			}
-			var importBlock = Array.from(newImports).filter(mod=>!imports.has(mod)).map(item=>`import "${item}";`).join('\n');
+			var importBlock = Array.from(newImports).filter(mod => !imports.has(mod)).map(item => `import "${item}";`).join('\n');
 			//console.log(importBlock);
 			magicString.prepend((importBlock + "\n\n"));
 
@@ -200,5 +187,5 @@ function polyfill(options) {
 	};
 }
 polyfill.default = polyfill;
-polyfill.__esModule=true;
+polyfill.__esModule = true;
 module.exports = polyfill;
